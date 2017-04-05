06b509f704163808ed0ea0a37608b63682f6a93a
DW31 <ik012982i9@H012982.NET>
2017-04-05 12:28:48 +0200

    cambios css js

---
+++ flask/static/botones.js
@@ -0,0 +1,31 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+       
+		$(document).ready( //siempre. para seleccionar objetos $... siempre estas dos primeras lineas para empezar
+                function(){     //estas dos lineas
+                      $("#idaClick").click(
+                      		function(){
+    							$("#ida").fadeTo(1000, 0.4);
+    							alert('Ha seleccionado billete de ida');
+    						}	
+
+                    );//click1
+
+                      $("#ivClick").click(
+                      		function(){
+    							$("#iv").fadeTo(1000, 0.4);
+    							alert('Ha seleccionado billete de ida y vuelta');
+    						}	
+
+                    );//click2
+
+                      $("#menClick").click(
+                      		function(){
+    							$("#men").fadeTo(1000, 0.4);
+    							alert('Ha seleccionado el billete mensual');
+    						}	
+
+                    );//click3
+
+                  }//primera funcion despues de ready
+                );//ready
+	
