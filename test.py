539621b8838f8f60cc653c175c87869f9b79a75e
DW31 <ik012982i9@H012982.NET>
2017-04-05 12:08:06 +0200

    prueba botones

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import a
+import index
+import funcionesrenfe as fr
 
 app = Flask(__name__)
 app.debug = True
@@ -7,7 +10,7 @@ app.debug = True
 @app.route("/")
 def hello():
     s = "q taaaal"
-    return render_template('index.html', name=s)
+    return render_template('prueba.html', name=s)
 
 @app.route("/alumnos")
 def lista_alumnos():
@@ -16,6 +19,22 @@ def lista_alumnos():
 @app.route("/nombre/<s>")
 def get_name(s):
     return render_template('index.html', name=s)
+
+@app.route("/suma/<x>/<y>")
+def suma():
+    x=5
+    y=2
+    print(x+y)
+    return render_template('prueba.html')
+
+#@app.route("/setopc/<opc>")
+#def setopc(opc):
+
+
+@app.route("/selec_dest/")
+def selec_dest():
+    return fr.zonas
+
 
 if __name__ == "__main__":
     app.run()
